22fa86ba3f65c8959355432129b4b2b79d5c3e73
James Wong <jameswong0517@gmail.com>
2025-02-18 10:03:26 +0800

    Code cleanup

--- src/main/java/org/example/hvvs/modules/admin/repository/SecurityStaffProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.hvvs.modules.admin.repository;
-
-import org.example.hvvs.model.SecurityStaffProfiles;
-import java.util.List;
-
-public interface SecurityStaffProfileRepository {
-    List<SecurityStaffProfiles> findAll();
-} 
